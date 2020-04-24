9a37e532cf7af61ed5f915446a413680652173d5
Jack Wang <w3cfed@gmail.com>
2020-04-24 20:50:38 +0800

    delete multi-pages config

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,5 @@
 const path = require("path");
 const resolve = dir => path.join(__dirname, dir);
-// const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
-const IS_PROD = true
-// publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : "./", // 默认'/'，部署应用包时的基本 URL,
-
-
-const glob = require("glob");
-const pagesInfo = require("./pages.config");
-const pages = {};
-
-glob.sync('./src/pages/**/main.js').forEach(entry => {
-  let chunk = entry.match(/\.\/src\/pages\/(.*)\/main\.js/)[1];
-  const curr = pagesInfo[chunk];
-  if (curr) {
-    pages[chunk] = {
-      entry,
-      ...curr,
-      chunk: ["chunk-vendors", "chunk-common", chunk]
-    }
-  }
-})
 
 module.exports = {
   chainWebpack: config => {
@@ -36,9 +16,5 @@ module.exports = {
       .set("@store", resolve("src/store"))
       .set("@layouts", resolve("src/layouts"))
       .set("@static", resolve("src/static"));
-    // 防止多页面打包卡顿
-    config.plugins.delete("named-chunks");
-    return config;
-  },
-  pages
+  }
 }
