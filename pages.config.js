81ea14cb279823e964c3e567f0a54294fb2b68d8
Jack Wang <w3cfed@gmail.com>
2020-04-24 20:24:08 +0800

    add multi-pages config

---
+++ pages.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'home': {
+    template: 'public/index.html',
+    filename: 'index.html',
+    title: '1024.Cool',
+  },
+  'admin': {
+    template: 'public/index.html',
+    filename: 'admin.html',
+    title: '后台管理',
+  },
+  'resume': {
+    template: 'public/index.html',
+    filename: 'resume.html',
+    title: '1024.Cool Markdown 程序员简历',
+  },
+  // 'pc/crm': {
+  //   template: 'public/index.html',
+  //   filename: 'pc-crm.html',
+  //   title: '预发服务',
+  // }
+}
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,24 @@ const resolve = dir => path.join(__dirname, dir);
 // const IS_PROD = ["production", "prod"].includes(process.env.NODE_ENV);
 const IS_PROD = true
 // publicPath: IS_PROD ? process.env.VUE_APP_PUBLIC_PATH : "./", // 默认'/'，部署应用包时的基本 URL,
+
+
+const glob = require("glob");
+const pagesInfo = require("./pages.config");
+const pages = {};
+
+glob.sync('./src/pages/**/main.js').forEach(entry => {
+  let chunk = entry.match(/\.\/src\/pages\/(.*)\/main\.js/)[1];
+  const curr = pagesInfo[chunk];
+  if (curr) {
+    pages[chunk] = {
+      entry,
+      ...curr,
+      chunk: ["chunk-vendors", "chunk-common", chunk]
+    }
+  }
+})
+
 module.exports = {
   chainWebpack: config => {
     // 添加别名
@@ -18,5 +36,9 @@ module.exports = {
       .set("@store", resolve("src/store"))
       .set("@layouts", resolve("src/layouts"))
       .set("@static", resolve("src/static"));
-  }
+    // 防止多页面打包卡顿
+    config.plugins.delete("named-chunks");
+    return config;
+  },
+  pages
 }
