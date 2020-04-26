cd041ccc29b9d7f59130f273d9da31f47548cd52
Jack Wang <w3cfed@gmail.com>
2020-04-26 19:47:38 +0800

    design i18n

---
+++ src/languages/languages.js
@@ -0,0 +1,17 @@
+import zhCN from './zh_CN'
+import enUS from './en_US'
+
+const languages = {
+  zh_CN: zhCN,
+  en_US: enUS,
+  setLanguage (language, array) {
+    array.map((val, index, array) => {
+      val = this[language][val]
+      array[index] = val
+    })
+    console.log(array)
+  },
+  lang: []
+}
+
+export default languages
---
+++ src/languages/zh_CN.js
@@ -0,0 +1,15 @@
+/**
+ * @w3cfed I18N for Chinese
+ * @w3cfed FE Development <w3cfed@gmail.com>
+ */
+
+const zhCN = {
+  english: '英文',
+  chinese: '中文简体',
+  edit: '编辑',
+  download: '下载',
+  pdf: 'PDF',
+  donate: '捐赠'
+}
+
+export default zhCN
