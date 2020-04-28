59fc0f5362e3ffc106943ab5a7e807e6221aa453
Jack Wang <w3cfed@gmail.com>
2020-04-29 03:23:35 +0800

    design i18n

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueI18n from 'vue-i18n'
 import '@scss/style.scss'
+import i18n from './i18n'
 
 Vue.use(VueI18n)
 
 Vue.config.productionTip = false
 
 new Vue({
+  i18n,
   render: h => h(App)
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,24 @@
-const path = require("path");
-const resolve = dir => path.join(__dirname, dir);
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
   chainWebpack: config => {
     // 添加别名
     config.resolve.alias
-      .set("@", resolve("src"))
-      .set("@assets", resolve("src/assets"))
-      .set("@image", resolve("src/assets/image"))
-      .set("@scss", resolve("src/assets/scss"))
-      .set("@languages", resolve("src/languages"))
-      .set("@components", resolve("src/components"));
+      .set('@', resolve('src'))
+      .set('@assets', resolve('src/assets'))
+      .set('@image', resolve('src/assets/image'))
+      .set('@scss', resolve('src/assets/scss'))
+      .set('@languages', resolve('src/languages'))
+      .set('@components', resolve('src/components'))
+  },
+
+  pluginOptions: {
+    i18n: {
+      locale: 'en',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: true
+    }
   }
 }
